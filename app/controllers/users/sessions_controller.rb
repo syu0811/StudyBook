d74e8762e2eb572dcc1eaf3b219d0e5c1ad2db68
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-09-13 18:50:29 +0900

    Add logout button
    ログアウトボタンを追加。それに伴い、ログアウト後のページ遷移部分を追加。

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -28,4 +28,8 @@ class Users::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(_resource)
     user_path(current_user.nickname)
   end
+
+  def after_sign_out_path_for(_resource)
+    new_user_session_path
+  end
 end
