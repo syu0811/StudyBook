f279c30ddae3d78b7f4d50a8a3d92365f095ef97
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-09-13 17:32:15 +0900

    Fix user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users, only: [:show]
+
+  root 'users#show'
 end
