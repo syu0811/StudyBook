c6d2871c157d4cbe9d7a1647a45315f277fe98b6
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-10-14 23:23:41 +0900

    タグの登録機能の追加
    テストケースも追加

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -2,7 +2,24 @@ module Admin
   class TagsController < ApplicationController
     before_action :get_tags, only: :index
 
+    def new
+      @tag = Tag.new
+    end
+
+    def create
+      @tag = Tag.new(tag_params)
+      if @tag.save
+        redirect_to admin_tags_path
+      else
+        render :new
+      end
+    end
+
     private
+
+    def tag_params
+      params.require(:tag).permit(:name)
+    end
 
     def get_tags
       @tags = Tag.all
