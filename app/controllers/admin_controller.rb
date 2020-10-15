282a9ea13945402b9848d02277e2e1f4dab9d529
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-10-15 21:35:29 +0900

    タグコントローラを管理者配下に移動(SB-34)
    Admin確認の関数をApplicaitonに移動
    タグの全てのアクションを管理者専用に変更

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,3 @@
 class AdminController < ApplicationController
   before_action :authenticate_admin!, only: [:index]
-
-  def authenticate_admin!
-    redirect_to user_path(current_user.nickname) unless current_user.admin
-  end
 end
