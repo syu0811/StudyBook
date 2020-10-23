f856012944599ea6fbec31ad6b2b1ef748d9b557
Suzumy <ryouga.0085@gmail.com>
2020-10-24 01:57:23 +0900

    rubocopによるエラーの修正/SB-25
    rubocop -aをすると、nicknameの前に_が必要と出たため、その通り修正した

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,11 +1,9 @@
 class NotesController < ApplicationController
   def index
-
     @notes = Note.includes(:user)
 
-    @notes.each do |nickname|
+    @notes.each do |_nickname|
       note.user.nickname
     end
-
   end
 end
