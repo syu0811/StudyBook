e92a4c3a4d043749836bbb23a0fbda07bb8be37b
Suzumy <ryouga.0085@gmail.com>
2020-10-26 10:18:11 +0900

    不要な機能を削除
    ログインしているユーザーのノートだけを見るようにする処理を削除した

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,5 @@
 class NotesController < ApplicationController
   def index
-    @users = current_user.id
-    @notes = Note.includes(:user).where('notes.user_id = ?', @users).references(:users)
-    # コピペプログラムで表示する内容をログインしているユーザーに限定することができた
+    @notes = Note.includes(:user)
   end
 end
