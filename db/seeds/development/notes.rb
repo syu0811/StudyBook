eb55cd8ed2263ebc325311db2b53569b29338fd2
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-10-28 22:12:00 +0900

    Seedsファイルの追加(SB-43)
    マイリストの追加に限界が来たので、シードファイルを一通り追加

---
+++ db/seeds/development/notes.rb
@@ -0,0 +1,9 @@
+CSV.foreach('db/seeds/development/csv/notes.csv', headers: true) do |row|
+  note = Note.new(
+    title: row['title'],
+    text: row['text'],
+    user: User.first
+  )
+
+  puts "note_save_error: #{note.errors}" unless note.save
+end
