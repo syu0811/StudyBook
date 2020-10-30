d6dc23c7ed661760fbe34cbed3f7bdddc976ae1d
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-10-31 00:06:17 +0900

    ユーザのサブスクライブ 中マイリストのモデル追加(SB-45)
    ユーザのサブスクライブ 中マイリストを格納するモデルを追加。
    サブスクライブ 中なので、他人のマイリストのみを追加する。

--- app/models/my_list.rb
+++ app/models/my_list.rb
@@ -4,6 +4,8 @@ class MyList < ApplicationRecord
 
   has_many :my_list_notes
   has_many :notes, through: :my_list_notes
+  has_many :user_subscribe_my_lists
+  has_many :subscribe_users, through: :user_subscribe_my_lists, source: :users
 
   validates :user_id, presence: true
   validates :category_id, presence: true
