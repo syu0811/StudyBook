ff05173418c071922fc88ab1a38754cc027683c5
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-10-30 16:02:40 +0900

    マイリスト詳細ページを追加(SB-66)

--- spec/requests/my_lists_request_spec.rb
+++ spec/requests/my_lists_request_spec.rb
@@ -14,5 +14,18 @@ RSpec.describe "MyLists", type: :request do
         expect(response).to have_http_status(:ok)
       end
     end
+
+    describe "GET /my_lists/:id" do
+      let(:my_list) { create(:my_list) }
+
+      before do
+        my_list
+      end
+
+      it 'ステータス OK が返ってくる' do
+        get my_list_path(my_list.id)
+        expect(response).to have_http_status(:ok)
+      end
+    end
   end
 end
