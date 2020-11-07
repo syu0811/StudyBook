25153fa71b9356909d675fcdba2b85f15c6be08c
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-11-07 13:20:39 +0900

    ESLintの追加(SB-65)

--- app/javascript/packs/text_area.js
+++ app/javascript/packs/text_area.js
@@ -1,10 +1,10 @@
 window.setRows = function (textarea, max_line_num = 5) {
-  var lines = textarea.value.split("\n");
+  var lines = textarea.value.split('\n');
   var rows = lines.length;
   if (lines.length > max_line_num) {
-    var result = "";
+    var result = '';
     for (var i = 0; i < max_line_num; i++) {
-      result += lines[i] += "\n";
+      result += lines[i] += '\n';
     }
     result = result.slice(0, -1);
     textarea.value = result;
