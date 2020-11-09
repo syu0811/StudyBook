b735983944fa5afc97f158939eece7f4014f53fb
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-11-09 22:09:18 +0900

    ノートに同期用の属性追加(SB-74)
    ノートに同期用に、file_pathを追加。
    guidを追加。

--- app/models/note.rb
+++ app/models/note.rb
@@ -11,4 +11,11 @@ class Note < ApplicationRecord
   validates :category_id, presence: true
   validates :title, presence: true, length: { maximum: 50 }
   validates :text, presence: true, length: { maximum: 30000 }
+  validates :file_path, length: { maximum: 255 }
+
+  before_create :add_uuid
+
+  def add_uuid
+    self.guid = SecureRandom.uuid
+  end
 end
