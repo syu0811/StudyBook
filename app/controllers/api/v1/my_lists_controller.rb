35784b5ad2d66e68c605ee10fcec8ed744359615
Suzumy <ryouga.0085@gmail.com>
2020-11-13 11:39:30 +0900

    必要のない比較の削除/SB-75
    authenticate_tokenの比較が必要なかったため削除した

--- app/controllers/api/v1/my_lists_controller.rb
+++ app/controllers/api/v1/my_lists_controller.rb
@@ -3,10 +3,8 @@ module Api
     class MyListsController < ApiBaseController
       protect_from_forgery # これがないとCSRF対策でpostが弾かれるっぽい?
       def response_mylists
-        if authenticate_token! != :bad_request
-          @my_lists = MyList.find(params[:id])
-          @my_notes = Note.find(params[:id])
-        end
+        @my_lists = MyList.find(params[:id])
+        @my_notes = Note.find(params[:id])
       end
     end
   end
