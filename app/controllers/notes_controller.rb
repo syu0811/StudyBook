e420747675db61426ba8f16b4ee3514ea5dc8450
syu0811 <codamayu494949@gmail.com>
2020-11-17 11:54:09 +0900

    マイリストのindexによるページ移動/SB-67
    indexを引数で与えて該当するノートのページへ移動する

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -7,14 +7,14 @@ class NotesController < ApplicationController
 
   private
 
-
-  def get_list 
-    @my_list_notes = MyListNote.joins(:note).select("my_list_notes.*, notes.title")
+  def get_list
+    @my_list_notes = MyListNote.joins(:note).select("my_list_notes.*, notes.*")
     @my_list_notes = @my_list_notes.where(my_list_id: params[:my_list_id]).order(index: "ASC")
+    @note = @my_list_notes.find_by(index: params[:index])
   end
 
   def get_note
-    get_list if params[:my_list_id].present?
     @note = Note.includes(:user, :category, :tags).find(params[:id])
+    get_list if params[:my_list_id].present?
   end
 end
--- app/javascript/packs/note.js
+++ app/javascript/packs/note.js
@@ -1,20 +1,22 @@
 window.onChange = function (value) {
   const url = new URL(location);
-  url.searchParams.set('category', value);
+  url.searchParams.set("category", value);
   location.href = url.toString();
 };
 
-window.onClick = function () {
+window.changePage = function (note_id, my_list_id, index) {
+  url = window.location.href.split("?")[0];
+  url = url.replace(/[0-9]+$/, note_id) + "?my_list_id=" + my_list_id + "&index=" + index
+  location.href = url.toString();
+};
+
+window.changeIndex = function (my_list_id, index, next) {
   const url = new URL(location);
-  url.searchParams.set('my_list_id', 1);
-  i = Number(url.searchParams.get("index"));
-  if (i == null){
-    url.searchParams.set('index', 1);
+  url.searchParams.set("my_list_id", my_list_id);
+  if (next){
+    url.searchParams.set("index", Number(index) + 1);
+  }else{
+    url.searchParams.set("index", Number(index) - 1);
   }
-  else{
-    url.searchParams.set('index', i + 1);
-  }
-  
   location.href = url.toString();
 };
-
