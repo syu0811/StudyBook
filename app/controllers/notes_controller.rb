2cc8c9e7b4388d889e25870611c61c067c50b801
syu0811 <codamayu494949@gmail.com>
2020-11-17 09:20:57 +0900

    マイリストのノート一覧を取得/SB-67
    my_list_idをクエリパラメータで取得し、指定したマイリストのノート一覧を取得する

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -7,7 +7,14 @@ class NotesController < ApplicationController
 
   private
 
+
+  def get_list 
+    @my_list_notes = MyListNote.joins(:note).select("my_list_notes.*, notes.title")
+    @my_list_notes = @my_list_notes.where(my_list_id: params[:my_list_id]).order(index: "ASC")
+  end
+
   def get_note
+    get_list if params[:my_list_id].present?
     @note = Note.includes(:user, :category, :tags).find(params[:id])
   end
 end
--- app/javascript/packs/note.js
+++ app/javascript/packs/note.js
@@ -3,3 +3,18 @@ window.onChange = function (value) {
   url.searchParams.set('category', value);
   location.href = url.toString();
 };
+
+window.onClick = function () {
+  const url = new URL(location);
+  url.searchParams.set('my_list_id', 1);
+  i = Number(url.searchParams.get("index"));
+  if (i == null){
+    url.searchParams.set('index', 1);
+  }
+  else{
+    url.searchParams.set('index', i + 1);
+  }
+  
+  location.href = url.toString();
+};
+
