e2d3c3215dad4a535dd50d8da973d48701e9648b
syu0811 <codamayu494949@gmail.com>
2020-11-18 10:42:39 +0900

    細かい修正ver.2/SB-67
    修正し忘れ

--- app/javascript/packs/note.js
+++ app/javascript/packs/note.js
@@ -1,5 +1,5 @@
 window.onChange = function (value) {
-  const url = new URL(location);
+  let url = new URL(location);
   url.searchParams.set("category", value);
   location.href = url.toString();
 };
