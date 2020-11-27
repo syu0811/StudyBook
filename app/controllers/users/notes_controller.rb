43a4ce9102c3c11abc3d4b102e41393fc2bc979a
syu0811 <codamayu494949@gmail.com>
2020-11-27 13:15:53 +0900

    ユーザーのノート一覧にソート機能を追加/SB-97
    ユーザーのノート一覧に更新順と作成順、名前順にソートする機能を追加

--- app/controllers/users/notes_controller.rb
+++ app/controllers/users/notes_controller.rb
@@ -5,6 +5,10 @@ module Users
       @directory_tree = @notes.directory_tree
       @notes = @notes.where(category_id: params[:category]) if params[:category].present?
       @notes = @notes.where('notes.directory_path LIKE ?', "#{params[:directory_path]}%")
+
+      sort = { "create" => "created_at DESC", "update" => "updated_at DESC", "name" => "title" }
+      @notes = @notes.order(sort["update"]) if params[:order].nil?
+      @notes = @notes.order(sort[params[:order]]) if params[:order].present?
     end
   end
 end
