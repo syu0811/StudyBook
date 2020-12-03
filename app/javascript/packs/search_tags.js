9477dded8c0825f733344b520c35e91b497479d4
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-12-03 21:59:52 +0900

    タグ検索が存在する時URLリロードの挙動を修正(SB-39)
    カテゴリや、ソート時、タグがリセットされる問題を修正(SB-39)

--- app/javascript/packs/search_tags.js
+++ app/javascript/packs/search_tags.js
@@ -9,6 +9,11 @@ function setSearchTagURL(tags = ['']) {
   });
 }
 
+function setTags(searchTags, tagName) {
+  let addHtml = `<div class="tag-search__tag" data-tag-name="${tagName}" onclick="removeSearchTag(event)">${tagName}</div>`;
+  searchTags.insertAdjacentHTML('beforeend', addHtml);
+}
+
 window.addSearchTag = function (e) {
   e.preventDefault();
   let tagName = e.srcElement.children[0].value;
@@ -18,8 +23,7 @@ window.addSearchTag = function (e) {
       return;
     }
   }
-  let addHtml = `<div class="tag-search__tag" data-tag-name="${tagName}" onclick="removeSearchTag(event)">${tagName}</div>`;
-  searchTags.insertAdjacentHTML('beforeend', addHtml);
+  setTags(searchTags, tagName);
   e.srcElement.children[0].value = '';
   let tags = [];
   for(let tag of searchTags.children) {
@@ -37,3 +41,16 @@ window.removeSearchTag = function(e) {
   }
   setSearchTagURL(tags);
 };
+
+window.addEventListener('DOMContentLoaded', function () {
+  let searchTags = document.getElementById('search_tags');
+  if(searchTags == undefined) {
+    return;
+  }
+  console.log(searchTags);
+
+  let url = new URL(location);
+  for(let tagName of url.searchParams.get('tags').split(',')) {
+    setTags(searchTags, tagName);
+  }
+});
