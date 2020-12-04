631980c319a0206f88155d24432a2308d41aaa75
syu0811 <codamayu494949@gmail.com>
2020-12-04 14:28:33 +0900

    細かい修正/SB-114
    細かい修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,7 +2,6 @@ class TopController < ApplicationController
   NUMBER_OF_NEW_NOTES = 50
 
   def index
-    @new_notes = Note.includes(:user, :tags, :category)
-    @new_notes = @new_notes.order(created_at: "DESC").limit(NUMBER_OF_NEW_NOTES)
+    @new_notes = Note.includes(:user, :tags, :category).order(created_at: "DESC").limit(NUMBER_OF_NEW_NOTES)
   end
 end
