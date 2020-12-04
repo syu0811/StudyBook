49c62e6a25694bd3a4cda61b2af6c69c9d3547ca
syu0811 <codamayu494949@gmail.com>
2020-12-04 14:23:52 +0900

    トップページに新着ノート一覧を追加/SB-114
    トップページに新着ノート一覧（すべてのユーザーのノート）を追加

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,8 @@
 class TopController < ApplicationController
+  NUMBER_OF_NEW_NOTES = 50
+
   def index
+    @new_notes = Note.includes(:user, :tags, :category)
+    @new_notes = @new_notes.order(created_at: "DESC").limit(NUMBER_OF_NEW_NOTES)
   end
 end
