b46a6da63fb4d8db5e2d2d7594b4b7d170456003
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-12-06 12:49:56 +0900

    プロフィール画像のファイルアップロードバグ修正(SB-64)
    ファイルアップロード時、キャンセルされた場合に、
    更新されない問題を修正

--- app/javascript/packs/user.js
+++ app/javascript/packs/user.js
@@ -2,14 +2,16 @@ window.userImageLoad = function(e) {
   let image = document.getElementById('user_image_upload');
   const files = e.target.files;	//ファイルを全部取得
   if(files.length > 0){		//1つ以上存在すればOK
+    window.userUploadImageFiles = files;
     const file = files[0];	//最初の物を取得
     const reader = new FileReader();	//ファイルを読み込むためのもの
     reader.onload = (e) => {	//ファイルがロードされたら動き出す。（そこまで停止している）
       let imageData = e.target.result;	//:src = に値が格納されるbase64形式で画像が格納される
       image.style.backgroundImage = `url(${imageData})`;
-      console.log('aaa');
     };
     reader.readAsDataURL(file);
+  } else if (window.userUploadImageFiles) {
+    e.target.files = window.userUploadImageFiles;
   }
 };
 
