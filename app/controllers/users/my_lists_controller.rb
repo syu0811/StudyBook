5ef4f7944dab237fb5c339af082730d081b38d80
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-12-08 12:15:08 +0900

    ユーザマイリストにサブスクライブボタン追加(SB-131)
    サブスクライブしたマイリストも表示されるように変更

--- app/controllers/users/my_lists_controller.rb
+++ app/controllers/users/my_lists_controller.rb
@@ -2,13 +2,18 @@ module Users
   class MyListsController < ApplicationController
     before_action :authenticate_user_nickname!, only: [:index]
     before_action :get_my_lists, only: [:index]
+    before_action :get_user_subscribe_my_list_ids, only: [:index]
 
     private
 
     def get_my_lists
-      @my_lists = MyList.includes(:category, :user).where(user: current_user)
+      @my_lists = MyList.includes(:category, :user).where(user: current_user).or(MyList.includes(:category, :user).where(id: SubscribeMyList.where(user: current_user).pluck(:my_list_id)))
       @my_lists = @my_lists.where(category_id: params[:category]) if params[:category].present?
       @my_lists = @my_lists.specified_order(params[:order])
+    end
+
+    def get_user_subscribe_my_list_ids
+      @user_subscribe_my_list_ids = current_user.subscribe_my_lists.pluck(:my_list_id)
     end
   end
 end
