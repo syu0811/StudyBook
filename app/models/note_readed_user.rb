ab63675235526418dba6b1d741bd9462bd71f59c
syu0811 <codamayu494949@gmail.com>
2020-12-14 17:59:49 +0900

    sqlの修正/SB-124
    sqlの修正

--- app/models/note_readed_user.rb
+++ app/models/note_readed_user.rb
@@ -6,20 +6,12 @@ class NoteReadedUser < ApplicationRecord
   validates :user_id, presence: true, uniqueness: { scope: :note_id }
 
   def self.get_reladed_notes_list(looking_note)
-    # 今見ているノートを見たことがあるユーザーたちが見ていたノート一覧を取得
     @looked_notes = NoteReadedUser.where(user_id: NoteReadedUser.where(note_id: looking_note.id))
-
-    # さらに取得したノート一覧から同じカテゴリーのノート一覧を取得
-    @related_notes = Note.includes(:user, :category, :tags).where(id: @looked_notes.pluck(:note_id), category_id: looking_note.category_id).where.not(id: looking_note.id)
-
-    # 同じカテゴリーのノート一覧からタグが一致しているノート一覧を取得
-    @tag_notes = NoteTag.where(tag_id: NoteTag.where(note_id: looking_note.id))
-    @related_notes = @related_notes.where(id: @tag_notes.pluck(:note_id)) if @tag_notes.present?
-    @related_notes
-  rescue  # 今見ているノートを見たことがあるユーザーが一人もいなかった場合、全体のノートをもとに関連ノートを取得する
     @related_notes = Note.includes(:user, :category, :tags).where(category_id: looking_note.category_id).where.not(id: looking_note.id)
+    @related_notes = Note.includes(:user, :category, :tags).where(id: @looked_notes.pluck(:note_id), category_id: looking_note.category_id).where.not(id: looking_note.id) if @looked_notes.present?
     @tag_notes = NoteTag.where(tag_id: NoteTag.where(note_id: looking_note.id))
     @related_notes = @related_notes.where(id: @tag_notes.pluck(:note_id)) if @tag_notes.present?
+
     @related_notes
   end
 end
