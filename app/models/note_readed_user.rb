0ad17f86a0b6dbb8e60f1a1613825c80fb289093
syu0811 <codamayu494949@gmail.com>
2020-12-14 11:52:31 +0900

    コード削減（不十分　12行/10行)、noteのshowに関連ノートの一覧表示の追加/SB-124 コードを削減（不十分） viewファイルの編集

--- app/models/note_readed_user.rb
+++ app/models/note_readed_user.rb
@@ -6,29 +6,22 @@ class NoteReadedUser < ApplicationRecord
   validates :user_id, presence: true, uniqueness: { scope: :note_id }
 
   def self.get_reladed_notes_list(looking_note)
-    # ログインしているユーザーが見ているノートを見たことがあるユーザー一覧を取得
-    @looked_users = NoteReadedUser.where(note_id: note_id)
-
-    # 取得したユーザー一覧からそのユーザーたちが見ていたノート一覧を取得
-    @looked_notes = NoteReadedUser.where(user_id: @looked_users.pluck(:user_id))
+    # 今見ているノートを見たことがあるユーザーたちが見ていたノート一覧を取得
+    @looked_notes = NoteReadedUser.where(user_id: NoteReadedUser.where(note_id: looking_note.id))
 
     # さらに取得したノート一覧から同じカテゴリーのノート一覧を取得
     @related_notes = Note.includes(:user, :category, :tags).where(id: @looked_notes.pluck(:note_id), category_id: looking_note.category_id).where.not(id: looking_note.id)
 
     # 同じカテゴリーのノート一覧からタグが一致しているノート一覧を取得
     @tag_notes = NoteTag.where(note_id: looking_note.id)
-    if @tag_notes.present?  # tagが一致しているノートがあったら
-      @tag_notes = NoteTag.where(tag_id: @tag_notes.pluck(:tag_id))
-      @related_notes = @related_notes.where(id: @tag_notes.pluck(:note_id))
-    end
+    @tag_notes = NoteTag.where(tag_id: @tag_notes.pluck(:tag_id)) if @tag_notes.present?
+    @related_notes = @related_notes.where(id: @tag_notes.pluck(:note_id)) if @tag_notes.present?
     @related_notes
-  rescue
+  rescue  # 今見ているノートを見たことがあるユーザーが一人もいなかった場合、全体のノートをもとに関連ノートを取得する
     @related_notes = Note.includes(:user, :category, :tags).where(category_id: looking_note.category_id).where.not(id: looking_note.id)
     @tag_notes = NoteTag.where(note_id: looking_note.id)
-    if @tag_notes.present?
-      @tag_notes = NoteTag.where(tag_id: @tag_notes.pluck(:tag_id))
-      @related_notes = @related_notes.where(id: @tag_notes.pluck(:note_id))
-    end
+    @tag_notes = NoteTag.where(tag_id: @tag_notes.pluck(:tag_id)) if @tag_notes.present?
+    @related_notes = @related_notes.where(id: @tag_notes.pluck(:note_id)) if @tag_notes.present?
     @related_notes
   end
 end
