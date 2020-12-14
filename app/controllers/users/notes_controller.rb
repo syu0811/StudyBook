06fe56873bfb942bbc872dd773822320cfcf879a
Suzumy <ryouga.0085@gmail.com>
2020-12-14 19:56:49 +0900

    ユーザのノートに検索機能を追加/(SB-95)
    タイトルの文字列から検索する機能を追加した。

--- app/controllers/users/notes_controller.rb
+++ app/controllers/users/notes_controller.rb
@@ -8,6 +8,8 @@ module Users
       @directory_tree = @notes.directory_tree
       @notes = @notes.where(category_id: params[:category]) if params[:category].present?
       @notes = @notes.where('notes.directory_path LIKE ?', "#{params[:directory_path]}%")
+      @notes = @notes.high_light_full_search(params[:q]) if params[:q].present?
+      @notes = @notes.tags_search(params[:tags]) if params[:tags].present?
       @notes = @notes.specified_order(params[:order])
       @pagy, @notes = pagy(@notes, items: ITEMS_PER_PAGE, link_extra: 'data-remote="true"')
     end
