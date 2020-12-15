4cc148e57d2eafc712d9a5c35bab6e5a8d396767
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-12-15 10:38:13 +0900

    ディレクトリツリ-Ajax対応(SB-118)
    デザインも追加

--- app/javascript/packs/directory_tree.js
+++ app/javascript/packs/directory_tree.js
@@ -1,3 +1,4 @@
+import { resetPage, ajax } from './search';
 window.toggleDirectory = function (e) {
   var ulist = e.srcElement.parentNode.parentNode.children;
   if (e.srcElement.classList.contains('close') == true) {
@@ -12,3 +13,26 @@ window.toggleDirectory = function (e) {
     e.srcElement.classList.add('close');
   }
 };
+
+function setActiveDirectoryClass(value) {
+  let directoryId = (value == '') ? 'directory_notes' : `directory_${value}`;
+  window.activeDirectoryElement = document.getElementById(directoryId);
+  if(window.activeDirectoryElement) {
+    window.activeDirectoryElement.classList.add('active-content');
+  }
+}
+window.setDirectoryPathParamsAjax = function (value) {
+  if(window.activeDirectoryElement) {
+    window.activeDirectoryElement.classList.remove('active-content');
+  }
+  setActiveDirectoryClass(value);
+  let url = new URL(location);
+  url.searchParams.set('directory_path', value);
+  resetPage(url);
+  ajax(url);
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  let url = new URL(location);
+  setActiveDirectoryClass(url.searchParams.get('directory_path'));
+});
--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,6 +1,21 @@
+import Axios from 'axios';
+
 export function resetPage(url) {
   url.searchParams.set('page', '1');
 }
+
+export function ajax(url) {
+  window.history.pushState(null, null, url.toString());
+  Axios.get(url.toString(), {headers: {'X-Requested-With': 'XMLHttpRequest'}}).then(response => {
+    eval(response.data);
+  });
+}
+
+window.setParams = function(key, value) {
+  let url = new URL(location);
+  url.searchParams.set(key, value);
+  window.history.pushState(null, null, url.toString());
+};
 
 window.onChange = function (value) {
   let url = new URL(location);
@@ -24,3 +39,24 @@ window.setSearchParams = function (e) {
   e.preventDefault();
 };
 
+window.setCategoryParamsAjax = function (value) {
+  let url = new URL(location);
+  url.searchParams.set('category', value);
+  resetPage(url);
+  ajax(url);
+};
+
+window.setSortParamsAjax = function (value) {
+  let url = new URL(location);
+  url.searchParams.set('order', value);
+  resetPage(url);
+  ajax(url);
+};
+
+window.setSearchParamsAjax = function (e) {
+  let url = new URL(location);
+  url.searchParams.set('q', e.srcElement.children.search_field.value);
+  resetPage(url);
+  ajax(url);
+  e.preventDefault();
+};
