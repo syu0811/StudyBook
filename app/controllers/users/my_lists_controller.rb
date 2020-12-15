88e020159ece441a2e01bbfc9b0c2d28445de775
Suzumy <ryouga.0085@gmail.com>
2020-12-15 14:05:52 +0900

    マイリストに検索機能を追加/(SB-96)
    マイリストに検索機能を追加した

--- app/controllers/users/my_lists_controller.rb
+++ app/controllers/users/my_lists_controller.rb
@@ -12,6 +12,7 @@ module Users
       @my_lists = MyList.includes(:category, user: { image_attachment: :blob }).where(user: current_user).or(MyList.includes(:category, user: { image_attachment: :blob }).where(id: SubscribeMyList.where(user: current_user).pluck(:my_list_id)))
       @my_lists = @my_lists.where(category_id: params[:category]) if params[:category].present?
       @my_lists = @my_lists.where(user_id: current_user.id) if params[:user] == 'true'
+      @my_lists = @my_lists.high_light_full_search(params[:q]) if params[:q].present?
       @my_lists = @my_lists.specified_order(params[:order])
       @pagy, @my_lists = pagy(@my_lists, items: ITEMS_PER_PAGE)
     end
