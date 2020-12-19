bd4a3a9d86bee3380ae3300ad9c10eeb886a5742
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-12-19 20:28:47 +0900

    グラフ表示の追加(SB-123)
    勉強履歴グラフの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action -> { authenticate_user_nickname!(:nickname) }, only: [:show]
   before_action :get_user, only: [:show, :edit, :update]
+  before_action :get_user_monthly_study_length, only: [:show]
 
   def update
     if @user.update(user_params)
@@ -18,5 +19,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:nickname, :image)
+  end
+
+  def get_user_monthly_study_length
+    @user_monthly_study_length = StudyLog.new(current_user.id).user_monthly_study_length
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,6 +21,7 @@ import './subscribe_my_list';
 import './search';
 import './search_tags';
 import './user';
+import './user_study_graph';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
