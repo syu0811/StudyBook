13aabca3e51f53dda0fbf7fae8fdab1874915b5d
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-12-20 23:03:53 +0900

    StudyLogのDBテスト追加(SB-123)
    正しく、ログが取得できないバグも修正

--- app/influxdb/study_log.rb
+++ app/influxdb/study_log.rb
@@ -12,37 +12,39 @@ class StudyLog < Influxdb::Base
 
   # 当月を含む過去12ヶ月間(12ヶ月前は含む)
   def user_monthly_study_length
-    result = get_logs_by_range(Time.zone.now.beginning_of_day.ago(1.year).iso8601, '1mo')
+    result = get_logs_by_range('1mo', 1.years)
     result.present? ? result : 13.times.map { 0 }
-  end
-
-  def get_logs_by_range(start_date, every)
-    query = "from(bucket: \"#{@client.bucket}\")
-      |> range(start: #{start_date}, stop: now())
-      |> filter(fn: (r) =>
-          r._measurement == \"#{@client.user_id}\")
-      |> window(every: #{every}, createEmpty: true)
-      |> sum()"
-    @client.query_api.query(query: query).map do |section|
-      section[1].records[0].value || 0
-    end
   end
 
   private
 
-  def call_write_api(logs, user_id)
-    write_api(data: import_study_data(logs, user_id))&.code == STATUS_CODE_OF_NO_CONTENT
+  def call_write_api(logs)
+    write_api(data: import_study_data(logs))&.code == STATUS_CODE_OF_NO_CONTENT
   end
 
   def import_study_data(logs)
     [].tap do |arr|
       logs.each do |log|
         log[:date_at] ||= Time.now.utc
+        log[:word_count] ||= 0
         arr << InfluxDB2::Point.new(name: @client.user_id)
                                .add_tag('note_id', log[:note_id])
                                .add_field('word_count', log[:word_count])
                                .time(log[:date_at], InfluxDB2::WritePrecision::NANOSECOND)
       end
     end
+  end
+
+  def get_logs_by_range(every, ago_time)
+    stop_time = Time.zone.now
+    start_time = stop_time.ago(ago_time)
+    query = "from(bucket: \"#{@client.bucket}\")
+      |> range(start: #{start_time.iso8601}, stop: #{stop_time.iso8601})
+      |> filter(fn: (r) =>
+          r._measurement == \"#{@client.user_id}\")
+      |> group(columns: [\"_measurement\"])
+      |> window(every: #{every}, createEmpty: true)
+      |> sum()"
+    @client.query_api.query(query: query).map { |section| section[1].records[0].value || 0 }
   end
 end
