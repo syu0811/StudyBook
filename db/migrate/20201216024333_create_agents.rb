3824f937e7af39c5082808503f7f453cfb300b16
Suzumy <ryouga.0085@gmail.com>
2020-12-24 22:14:16 +0900

    Draft:わからないなりに色々やった結果一応DBができたかもしれない
    一応DBを作っててはみたけどtokenカラムを外部キーにすることができませんでした。

--- db/migrate/20201216024333_create_agents.rb
+++ db/migrate/20201216024333_create_agents.rb
@@ -1,6 +1,8 @@
 class CreateAgents < ActiveRecord::Migration[6.0]
   def change
     create_table :agents do |t|
+      t.references :user, foreign_key: true, null: false
+      t.string :token, index: true, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -39,8 +39,12 @@ ActiveRecord::Schema.define(version: 2020_12_16_024333) do
   end
 
   create_table "agents", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "token", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["token"], name: "index_agents_on_token"
+    t.index ["user_id"], name: "index_agents_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -163,6 +167,7 @@ ActiveRecord::Schema.define(version: 2020_12_16_024333) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "agents", "users"
   add_foreign_key "deleted_notes", "users"
   add_foreign_key "my_list_notes", "my_lists"
   add_foreign_key "my_list_notes", "notes"
