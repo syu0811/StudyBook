dc1ab38a1932ead9d2ce6e4d3c3a905fcdda8210
BMuscle <kd1242488@st.kobedenshi.ac.jp>
2020-12-27 13:31:01 +0900

    生のmdデータが表示される問題を修正(SB-153)

--- app/javascript/packs/marked.js
+++ app/javascript/packs/marked.js
@@ -3,10 +3,7 @@ import hljs from 'highlight.js';
 
 window.addEventListener('DOMContentLoaded', function () {
   marked.setOptions({
-    // code要素にdefaultで付くlangage-を削除
-    // langPrefix: '',
     langPrefix: 'hljs language-',
-    // highlightjsを使用したハイライト処理を追加
     highlight: function(code, lang) {
       return hljs.highlightAuto(code, [lang]).value;
     }
