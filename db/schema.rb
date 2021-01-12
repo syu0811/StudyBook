64c3a9f068b632c7762dcf9de998d71d7f2fcc20
Suzumy <ryouga.0085@gmail.com>
2021-01-12 12:18:26 +0900

    usersテーブルのtokenカラムを削除/(SB-87)
    usersテーブルからtokenカラムを削除するmigrationファイルを作成、実行し、tokenカラムを削除した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_024333) do
+ActiveRecord::Schema.define(version: 2021_01_12_031543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -161,7 +161,6 @@ ActiveRecord::Schema.define(version: 2020_12_16_024333) do
     t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "token"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
